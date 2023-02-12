caecd480b8c2f0dc15334c768034bee3cbbbbce8
Andrés Valle <>
2023-02-12 22:02:58 +0100

    Push changes into dev | 1 file changed, 6 insertions(+), 9 deletions(-)

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const Crawler = require('./classes/Crawler');
 
 const crawler = new Crawler('https://www.census.gov/economic-indicators/');
 
-async function main() {
-    const element = await crawler.select("#retail_sales");
+async function main(selector) {
+    const element = await crawler.select(selector);
     const html = element.html();
-    if (html.includes(
-        `<h2 data-tooltip-bottom="Advance Monthly Retail Sales">`
-    ) && html.includes(
-        `<h3>January 18th, 2023</h3>`
-    )) {
-        console.log('Test passed');
+
+    if (html.includes(`<h2 data-tooltip-bottom="Advance Monthly Retail Sales">`)
+        && html.includes(`<h3>January 18th, 2023</h3>`)) {
         return true;
     }
 }
 
-main();
+main("#retail_sales");
