3d3b7763a43dfecaa7b8f016c5eb4ed5e67644fc
Andrés Valle <>
2023-02-16 16:12:28 +0100

    Push changes into dev | 3 files changed, 57 insertions(+), 70 deletions(-)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
-const Crawler = require('./classes/Crawler');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-const census = new Crawler("https://www.census.gov/economic-indicators/");
-census.selector = "#retail_sales";
-census.delay = 1000;
-census.terms = [
-    `<h2 data-tooltip-bottom="Advance Monthly Retail Sales">`,
-    `<h3>February 15th, 2023</h3>`
-];
-census.run();
+class Crawler {
+    constructor(url, xml = false) {
+        if (!url) {
+            throw new Error('URL is required');
+        }
 
+        this.url = url;
+        this.delay = 250;
+        this.selector = "body";
+        this.terms = [];
+        this.xml = xml;
+    }
 
+    async html() {
+        const { data, status } = await axios.get(this.url);
+        if (status != 200) console.error(status);
+        return data;
+    }
+
+    async select() {
+        const $ = cheerio.load(await this.html(), { xmlMode: this.xml });
+        return $(this.selector).html();
+    }
+
+    async wait() {
+        return new Promise(resolve => (
+            setTimeout(resolve, this.delay)
+        ));
+    }
+
+    async search() {
+        const element = await this.select(this.selector);
+        const check = [];
+
+        this.terms.map(term => {
+            const date = new Date().toISOString();
+            check.push(element.includes(term));
+            console.log(
+                `${date} |`,
+                `Searching: "${term}" |`,
+                `${element.includes(term) ? "Found!" : "Miss"}`
+            );
+        })
+        return check.every(term => term === true);
+    }
+
+    async run() {
+        let found = false;
+        while (found === false) {
+            found = await this.search();
+            await this.wait();
+        }
+        return found;
+    }
+}
+
+module.exports = Crawler;
